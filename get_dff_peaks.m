d0e7de4032ec55dc78c98594dd1a84ba7e0ca63e
David Ackerman <ackermand@janelia.hhmi.org>
2021-10-07 10:13:11 -0400

    Add function to get peaks in DFF

---
+++ get_dff_peaks.m
@@ -0,0 +1,16 @@
+function [pks, locs, widths, proms] = get_dff_peaks(DFF)
+
+pks = cell(size(DFF,1),1);
+locs = cell(size(DFF,1),1);
+widths = cell(size(DFF,1),1);
+proms = cell(size(DFF,1),1);
+imageData = zeros(size(DFF));
+
+for i=1:size(DFF,1)
+    [pks{i},locs{i},widths{i},proms{i}] = findpeaks(DFF(i,:),'MinPeakProminence',0.03);
+    imageData(i,locs{i}) = 1;
+end
+
+imshow(imageData);
+xlabel("Frame");
+ylabel("Cluster");
